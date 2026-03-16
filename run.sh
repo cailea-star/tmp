b15ff4000c649c23bee2d99215ac1ca33dc88471
cailea <cailea@qq.com>
2026-03-16 14:41:12 +0800

    feat: Update run.hk and run.sh with new parameter values for improved execution

--- run.sh
+++ run.sh
@@ -9,13 +9,13 @@ for ((N=157;N<=157;N=N+2)); do
 
 #N=74
 
-run.hk $Z $Z $N $N Ds267-HKpr1n2p100
+run.hk $Z $Z $N $N Ds269-HKpr1n2p16
 
 
 
 cd mp
 
-run.mp $Z $Z $N $N Ds267-HKpr1n2p100
+run.mp $Z $Z $N $N Ds269-HKpr1n2p16
 
 
 
